d7d12c597f4b4fc615a8d7d5806565609ee18ccf
No0ne <github@exa.solar>
2022-09-27 00:37:21 +0200

    cleanup finished

--- ps2pico.c
+++ ps2pico.c
@@ -28,17 +28,46 @@ uint8_t const hid2ps2[] = {
 // 0183, 018a, 0192, 0194, 0221, 0223, 0224, 0225, 0226, 0227, 022a
 // 0x50, 0x48, 0x2b, 0x40, 0x10, 0x3a, 0x38, 0x30, 0x28, 0x20, 0x18
 
-//uint8_t state = 0;
-
+bool irq_enabled = true;
+bool kbd_enabled = true;
 uint8_t kbd_addr = 0;
 uint8_t kbd_inst = 0;
 
+bool receiving = false;
+bool repeating = false;
+uint32_t repeat_us = 35000;
+uint16_t delay_ms = 250;
+alarm_id_t repeater;
+
+uint8_t prev_rpt[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+uint8_t prev_ps2 = 0;
 uint8_t resend = 0;
+uint8_t repeat = 0;
 
-bool irq_enabled = true;
+bool blink = false;
+int64_t blink_callback(alarm_id_t id, void *user_data) {
+  if(kbd_addr) {
+    if(blink) {
+      uint8_t static value = 7; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1);
+      blink = false;
+      return 500000;
+    } else {
+      uint8_t static value = 0; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1);
+    }
+  }
+  
+  return 0;
+}
 
-//uint8_t ps2in[] = { 0, 0 };
-//uint8_t ps2out[] = { 0, 0, 0, 0 };
+int64_t repeat_callback(alarm_id_t id, void *user_data) {
+  if(repeat) {
+    repeating = true;
+    return repeat_us;
+  }
+  
+  repeater = 0;
+  return 0;
+}
 
 void ps2_cycle_clock() {
   sleep_us(20);
@@ -78,7 +107,7 @@ bool ps2_send(uint8_t data) {
   return true;
 }
 
-void ps2_send_e0(uint8_t data) {
+void maybe_send_e0(uint8_t data) {
   if(data == 0x46 ||
      data >= 0x48 && data <= 0x52 ||
      data == 0x54 || data == 0x58 ||
@@ -88,75 +117,17 @@ void ps2_send_e0(uint8_t data) {
   }
 }
 
-void ps2_process(uint8_t data) {
-  /*if(received == 0xff) {
-    while(!ps2_send(0xaa));
-    enabled = true;
-    blink = true;
-    add_alarm_in_ms(100, blink_callback, NULL, false);
-    return;
-    
-  } else if(received == 0xee) {
-    ps2_send(0xee);
-    return;
-    
-  } else if(received == 0xfe) {
-    ps2_send(resend);
-    return;
-    
-  } else if(received == 0xf2) {
-    ps2_send(0xfa);
-    ps2_send(0xab);
-    ps2_send(0x83);
-    return;
-    
-  } else if(received == 0xf4) {
-    enabled = true;
-    
-  } else if(received == 0xf5 || received == 0xf6) {
-    enabled = received == 0xf6;
-    repeatus = 35000;
-    delayms = 250;
-    uint8_t static value = 0; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1);
-  }
-  
-  if(prevps2 == 0xf3) {
-    repeatus = received & 0x1f;
-    delayms = received & 0x60;
-    
-    repeatus = 35000 + repeatus * 15000;
-    
-    if(delayms == 0x00) delayms = 250;
-    if(delayms == 0x20) delayms = 500;
-    if(delayms == 0x40) delayms = 750;
-    if(delayms == 0x60) delayms = 1000;
-    
-  } else if(prevps2 == 0xed) {
-    if(received == 1) { uint8_t static value = 4; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-    if(received == 2) { uint8_t static value = 1; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-    if(received == 3) { uint8_t static value = 5; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-    if(received == 4) { uint8_t static value = 2; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-    if(received == 5) { uint8_t static value = 6; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-    if(received == 6) { uint8_t static value = 3; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-    if(received == 7) { uint8_t static value = 7; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
-                      { uint8_t static value = 0; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); }
-  }
-  
-  prevps2 = received;
-  ps2_send(0xfa);*/
-}
-
 void ps2_receive() {
   irq_enabled = false;
   board_led_write(1);
   
-  uint16_t data = 0;
   uint16_t bit = 1;
+  uint8_t data = 0;
   uint8_t parity = 1;
   
   ps2_cycle_clock();
   
-  while(bit < 0x0100) {
+  while(bit < 0x100) {
     if(gpio_get(DTIN)) {
       data = data | bit;
       parity = parity ^ 1;
@@ -168,165 +139,222 @@ void ps2_receive() {
     ps2_cycle_clock();
   }
   
-  uint8_t rparity = gpio_get(DTIN);
+  parity = gpio_get(DTIN) == parity;
   ps2_cycle_clock();
   
-  /*sleep_us(CLKHALF);
-  gpio_put(DTOUT, !0);
-  gpio_put(CLKOUT, !0); sleep_us(CLKFULL);
-  gpio_put(CLKOUT, !1); sleep_us(CLKHALF);
-  gpio_put(DTOUT, !1);*/
-  
-  gpio_put(DTOUT, !0);
-  ps2_cycle_clock();
+  ps2_set_bit(0);
   gpio_put(DTOUT, !1);
   
   irq_enabled = true;
   board_led_write(0);
   
-  if(parity == rparity) {
-    ps2_process(data & 0xff);
-  } else {
+  if(!parity) {
     ps2_send(0xfe);
-  }
-}
-
-/*alarm_id_t repeater;
-uint32_t repeatus = 35000;
-uint16_t delayms = 250;
-uint8_t repeat = 0;
-bool repeating = false;
-bool enabled = true;
-bool blink = false;
-
-uint8_t prevhid[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-uint8_t prevps2 = 0;
-
-int64_t blink_callback(alarm_id_t id, void *user_data) {
-  if(blink) {
-    blink = false;
-    add_alarm_in_ms(1000, blink_callback, NULL, false);
-    uint8_t static value = 7; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1);
-  } else {
-    uint8_t static value = 0; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1);
-  }
-}
-
-int64_t repeat_callback(alarm_id_t id, void *user_data) {
-  if(repeat) {
-    repeating = true;
-    return repeatus;
+    return;
   }
   
-  repeater = 0;
-  return 0;
-}*/
+  switch(prev_ps2) {
+    case 0xf3:
+      prev_ps2 = 0;
+      repeat_us = data & 0x1f;
+      delay_ms = data & 0x60;
+      
+      repeat_us = 35000 + repeat_us * 15000;
+      
+      if(delay_ms == 0x00) delay_ms = 250;
+      if(delay_ms == 0x20) delay_ms = 500;
+      if(delay_ms == 0x40) delay_ms = 750;
+      if(delay_ms == 0x60) delay_ms = 1000;
+    break;
+    
+    case 0xed:
+      prev_ps2 = 0;
+      if(data == 1) { uint8_t static value = 4; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+      if(data == 2) { uint8_t static value = 1; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+      if(data == 3) { uint8_t static value = 5; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+      if(data == 4) { uint8_t static value = 2; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+      if(data == 5) { uint8_t static value = 6; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+      if(data == 6) { uint8_t static value = 3; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+      if(data == 7) { uint8_t static value = 7; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); } else
+                    { uint8_t static value = 0; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1); }
+    break;
+    
+    default:
+      switch(data) {
+        case 0xff:
+          ps2_send(0xfa);
+          
+          kbd_enabled = true;
+          blink = true;
+          add_alarm_in_ms(1, blink_callback, NULL, false);
+          
+          sleep_us(5000);
+          ps2_send(0xaa);
+          
+          return;
+        break;
+        
+        case 0xfe:
+          ps2_send(resend);
+          return;
+        break;
+        
+        case 0xee:
+          ps2_send(0xee);
+          return;
+        break;
+        
+        case 0xf2:
+          ps2_send(0xfa);
+          ps2_send(0xab);
+          ps2_send(0x83);
+          return;
+        break;
+        
+        case 0xf3:
+        case 0xed:
+          prev_ps2 = data;
+        break;
+        
+        case 0xf4:
+          kbd_enabled = true;
+        break;
+        
+        case 0xf5:
+        case 0xf6:
+          kbd_enabled = data == 0xf6;
+          repeat_us = 35000;
+          delay_ms = 250;
+          uint8_t static value = 0; tuh_hid_set_report(kbd_addr, kbd_inst, 0, HID_REPORT_TYPE_OUTPUT, (void*)&value, 1);
+        break;
+      }
+    break;
+  }
+  
+  ps2_send(0xfa);
+}
 
 void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len) {
   
   if(tuh_hid_interface_protocol(dev_addr, instance) == HID_ITF_PROTOCOL_KEYBOARD) {
-    tuh_hid_receive_report(dev_addr, instance);
-    
     kbd_addr = dev_addr;
     kbd_inst = instance;
     
-    //blink = true;
-    //add_alarm_in_ms(100, blink_callback, NULL, false);
+    blink = true;
+    add_alarm_in_ms(1, blink_callback, NULL, false);
+    
+    tuh_hid_receive_report(dev_addr, instance);
   }
   
 }
 
-void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
-
+void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance) {
   if(dev_addr == kbd_addr && instance == kbd_inst) {
-    if(enabled) {
-      board_led_write(1);
-      
-      if(report[0] != prevhid[0]) {
-        uint8_t rbits = report[0];
-        uint8_t pbits = prevhid[0];
-        
-        for(uint8_t j = 0; j < 8; j++) {
-          
-          if((rbits & 0x01) != (pbits & 0x01)) {
-            if(j > 2 && j != 5) ps2_send(0xe0);
-            
-            if(rbits & 0x01) {
-              ps2_send(mod2ps2[j]);
-            } else {
-              ps2_send(0xf0);
-              ps2_send(mod2ps2[j]);
-            }
-          }
-          
-          rbits = rbits >> 1;
-          pbits = pbits >> 1;
-          
-        }
-        
-        prevhid[0] = report[0];
-      }
-      
-      for(uint8_t i = 2; i < 8; i++) {
-        if(prevhid[i]) {
-          bool brk = true;
-          
-          for(uint8_t j = 2; j < 8; j++) {
-            if(prevhid[i] == report[j]) {
-              brk = false;
-              break;
-            }
-          }
-          
-          if(brk) {
-            if(prevhid[i] == 0x48) continue;
-            //repeat = 0;
-            
-            ps2_send_e0(prevhid[i]);
-            ps2_send(0xf0);
-            ps2_send(hid2ps2[prevhid[i]]);
-          }
-        }
-        
-        if(report[i]) {
-          bool make = true;
-          
-          for(uint8_t j = 2; j < 8; j++) {
-            if(report[i] == prevhid[j]) {
-              make = false;
-              break;
-            }
-          }
-          
-          if(make) {
-            if(report[i] == 0x48) {
-              ps2_send(0xe1); ps2_send(0x14); ps2_send(0x77); ps2_send(0xe1);
-              ps2_send(0xf0); ps2_send(0x14); ps2_send(0xf0); ps2_send(0x77);
-              continue;
-            }
-            
-            /*repeat = report[i];
-            if(repeater) cancel_alarm(repeater);
-            repeater = add_alarm_in_ms(delayms, repeat_callback, NULL, false);*/
-            
-            ps2_send_e0(report[i]);
-            ps2_send(hid2ps2[report[i]]);
-          }
-        }
-        
-        prevhid[i] = report[i];
-      }
-      
-      board_led_write(0);
+    kbd_addr = 0;
+    kbd_inst = 0;
+  }
+}
+
+void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
+  if(dev_addr == kbd_addr && instance == kbd_inst) {
+    
+    if(!kbd_enabled) {
+      tuh_hid_receive_report(dev_addr, instance);
+      return;
     }
     
+    board_led_write(1);
+    
+    if(report[0] != prev_rpt[0]) {
+      uint8_t rbits = report[0];
+      uint8_t pbits = prev_rpt[0];
+      
+      for(uint8_t j = 0; j < 8; j++) {
+        
+        if((rbits & 0x01) != (pbits & 0x01)) {
+          if(j > 2 && j != 5) ps2_send(0xe0);
+          
+          if(rbits & 0x01) {
+            ps2_send(mod2ps2[j]);
+          } else {
+            ps2_send(0xf0);
+            ps2_send(mod2ps2[j]);
+          }
+        }
+        
+        rbits = rbits >> 1;
+        pbits = pbits >> 1;
+        
+      }
+      
+      prev_rpt[0] = report[0];
+    }
+    
+    for(uint8_t i = 2; i < 8; i++) {
+      if(prev_rpt[i]) {
+        bool brk = true;
+        
+        for(uint8_t j = 2; j < 8; j++) {
+          if(prev_rpt[i] == report[j]) {
+            brk = false;
+            break;
+          }
+        }
+        
+        if(brk) {
+          if(prev_rpt[i] == 0x48) continue;
+          repeat = 0;
+          
+          maybe_send_e0(prev_rpt[i]);
+          ps2_send(0xf0);
+          ps2_send(hid2ps2[prev_rpt[i]]);
+        }
+      }
+      
+      if(report[i]) {
+        bool make = true;
+        
+        for(uint8_t j = 2; j < 8; j++) {
+          if(report[i] == prev_rpt[j]) {
+            make = false;
+            break;
+          }
+        }
+        
+        if(make) {
+          if(report[i] == 0x48) {
+            
+            if(report[0] & 0x1 || report[0] & 0x10) {
+              ps2_send(0xe0); ps2_send(0x7e); ps2_send(0xe0); ps2_send(0xf0); ps2_send(0x7e);
+            } else {
+              ps2_send(0xe1); ps2_send(0x14); ps2_send(0x77); ps2_send(0xe1);
+              ps2_send(0xf0); ps2_send(0x14); ps2_send(0xf0); ps2_send(0x77);
+            }
+            
+            continue;
+          }
+          
+          repeat = report[i];
+          if(repeater) cancel_alarm(repeater);
+          repeater = add_alarm_in_ms(delay_ms, repeat_callback, NULL, false);
+          
+          maybe_send_e0(report[i]);
+          ps2_send(hid2ps2[report[i]]);
+        }
+      }
+      
+      prev_rpt[i] = report[i];
+    }
+    
+    board_led_write(0);
     tuh_hid_receive_report(dev_addr, instance);
+    
   }
 }
 
 void irq_callback(uint gpio, uint32_t events) {
   if(irq_enabled && !gpio_get(DTIN)) {
-    //state = 1;
+    receiving = true;
   }
 }
 
@@ -350,19 +378,18 @@ void main() {
   while(true) {
     tuh_task();
     
-    /* switch(state) {
-      case 1: ps2_receive(); break;
+    if(receiving) {
+      receiving = false;
+      ps2_receive();
     }
     
-    state = 0; */
-    
-    /*if(repeating) {
+    if(repeating) {
       repeating = false;
       
       if(repeat) {
-        ps2_send_e0(repeat);
+        maybe_send_e0(repeat);
         ps2_send(hid2ps2[repeat]);
       }
-    }*/
+    }
   }
 }
